754127e2655a4974d7dedb797681fa31dcebf7fb
Steve Farrell <sparticlesteve@gmail.com>
2021-09-27 11:34:16 -0700

    update pm shifter to 21.08

--- scripts/submit_tf2_hvd_pm_shifter.sh
+++ scripts/submit_tf2_hvd_pm_shifter.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-image=nersc/tensorflow:ngc-21.03-tf2-v0
+image=nersc/tensorflow:ngc-21.08-tf2-v1
 
 sbatch -N 1 --image=$image scripts/tf2_hvd_pm_shifter.sh
 sbatch -N 2 --image=$image scripts/tf2_hvd_pm_shifter.sh
